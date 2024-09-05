90a420393a15c564b181fb9927e39c06bf333ef2
Vijay Athithya <109908573+VijayKL01@users.noreply.github.com>
2024-09-06 01:16:12 +0530

    084 Domino.cpp

--- Ladder 11/084 Domino.cpp
+++ Ladder 11/084 Domino.cpp
@@ -1,25 +1,22 @@
-#include <iostream>
-using namespace std;
+def main():
+    n = int(input())
+    l, r = 0, 0
+    s = False
+    for _ in range(n):
+        x, y = map(int, input().split())
+        if n == 1 and (x % 2 + y % 2) == 1:
+            print(-1)
+            return
+        if (x % 2 + y % 2) == 1:
+            s = True
+        l += x
+        r += y
+    if l % 2 == 0 and r % 2 == 0:
+        print(0)
+    elif l % 2 == 1 and r % 2 == 1 and s:
+        print(1)
+    else:
+        print(-1)
 
-int main ()
-{
-    int n, l(0), r(0);
-    cin >> n;
-    bool s(false);
-    for (int x, y; cin >> x >> y;)
-    {
-        if (n == 1 and x % 2 + y % 2 == 1)
-        {
-            cout << -1;
-            return 0;
-        }
-        if (y % 2 + x % 2 == 1)
-            s = true;
-        l += x;
-        r += y;
-    }
-    if (l % 2 == 0 and r % 2 == 0) cout << 0 << endl;
-    else if (l % 2 == 1 and r % 2 == 1 and s) cout << 1 << endl;
-    else cout << -1 << endl;
-    return 0;
-}
+if __name__ == "__main__":
+    main()
