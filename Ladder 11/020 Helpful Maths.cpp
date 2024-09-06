f3ba9c3ce6db3c3a56778c6a32e0086b4cb4658f
Vijay Athithya <109908573+VijayKL01@users.noreply.github.com>
2024-09-06 16:38:44 +0530

    Update 020 Helpful Maths.cpp

--- Ladder 11/020 Helpful Maths.cpp
+++ Ladder 11/020 Helpful Maths.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main()
 {
-    string s;
-    cin>>s;
-    for(int i=0;i<s.length();i+=2)
+    string sr;
+    cin>>sr;
+    for(int i=0;i<sr.length();i+=2)
     {
-      for(int j=0;j<s.length()-1;j+=2){
-        if(s[j]>s[j+2])
+      for(int j=0;j<sr.length()-1;j+=2){
+        if(sr[j]>sr[j+2])
         {
-           char temp = s[j];
-           s[j]=s[j+2];
-           s[j+2]=temp;
+           char temp = sr[j];
+           sr[j]=sr[j+2];
+           sr[j+2]=temp;
         }
        }
     }
-    cout<<s;
+    cout<<sr;
     return 0;
 }
