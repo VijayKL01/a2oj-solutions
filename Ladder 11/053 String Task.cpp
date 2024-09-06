5c8fbeb44fbe0676ea869bfc98284bdbb511d155
Vijay Athithya <109908573+VijayKL01@users.noreply.github.com>
2024-09-06 21:59:04 +0530

    Update 053 String Task.cpp

--- Ladder 11/053 String Task.cpp
+++ Ladder 11/053 String Task.cpp
@@ -1,20 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
+
 int main() {
-    string a,c,d;
-    cin>>a;
-    int b=a.length();
-    for (int i=0;i<b;i++)
-    {
-        a[i]=tolower(a[i]);
-        if (a[i]=='a' || a[i]=='e' ||a[i]=='i' ||a[i]=='o' ||a[i]=='u'||a[i]=='y')
-        continue;
-        else
-        {
-        c=a[i];
-        d=d+'.'+c;
+    string a;
+    cin >> a;
+    int b = a.length();
+    string d = "";
+
+    for (int i = 0; i < b; i++) {
+        a[i] = tolower(a[i]);
+        if (!strchr("aeiouy", a[i])) {
+            d += "." + a[i];
         }
     }
-    cout<<d;
+
+    cout << d << endl;
+
     return 0;
 }
